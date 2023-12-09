ef9dc9c8c8533b4ec7c96bc290c3d3e35e95b8bb
Grisha-NoSense <Devimp15@mail.ru>
2023-12-09 16:47:30 +0300

    -HomeWork6

--- Printer.java
+++ Printer.java
@@ -1,55 +1,30 @@
-import java.util.Arrays;
-
-class HeapSort {
-    public static void buildTree(int[] tree, int sortLength) {
-        // Для всех вершин дерева начиная с середины сортируемой части массива
-        // (движемся влево до нулевого индекса)
-        for (int i = sortLength / 2 - 1; i >= 0; i--) {
-            int maxIndex;
-            // Если у вершины два листа, выбираем больший
-            if (i * 2 + 2 <= sortLength - 1) {
-                if (tree[i * 2 + 2] > tree[i * 2 + 1]) maxIndex = i * 2 + 2;
-                else maxIndex = i * 2 + 1;
-                // Иначе один лист
-            } else maxIndex = i * 2 + 1;
-            // Сравниваем лист с максимальным значением с вершиной, при большем значении
-            // дочернего элемента меняем его местами с вершиной
-            if (tree[i] < tree[maxIndex]) {
-                int temp = tree[i];
-                tree[i] = tree[maxIndex];
-                tree[maxIndex] = temp;
-            }
+class Expr {
+    public static String expr(char a) throws Exception {
+// Введите свое решение ниже
+        if (a == ' ') {
+            throw new Exception("Пустая строка введена.");
+        } else {
+            return "Result: Your input was - " + a;
         }
     }
-
-    public static void heapSort(int[] sortArray, int sortLength) {
-        // Выход из рекурсии: длина сортируемой части равна 0
-        if (sortLength == 0) return;
-        // Выстраивание элементов части массива в виде сортирующего дерева
-        buildTree(sortArray, sortLength);
-        // Замена местами первого и последнего элемента в сортируемой части
-        int temp = sortArray[0];
-        sortArray[0] = sortArray[sortLength - 1];
-        sortArray[sortLength - 1] = temp;
-        // Рекурсивно построение дерева и замена элементов для n - 1 элементов массива
-        heapSort(sortArray, sortLength - 1);
-    }
 }
+// Не удаляйте этот класс - он нужен для вывода результатов на экран и проверки
 
 public class Printer {
     public static void main(String[] args) {
-        int[] initArray;
+        char a;
 
         if (args.length == 0) {
-            initArray = new int[]{17, 32, 1, 4, 25, 17, 0, 3, 10, 7, 64, 1};
+            a = 'J'; // Значение по умолчанию, если аргументы не были предоставлены
         } else {
-            initArray = Arrays.stream(args[0].split(" ")).mapToInt(Integer::parseInt).toArray();
+            a = args[0].charAt(0); // Преобразуйте первый аргумент командной строки в символ
         }
 
-        System.out.println("Initial array:");
-        System.out.println(Arrays.toString(initArray));
-        HeapSort.heapSort(initArray, initArray.length);
-        System.out.println("Sorted array:");
-        System.out.println(Arrays.toString(initArray));
+        try {
+            String result = Expr.expr(a);
+            System.out.println("Result: " + result);
+        } catch (Exception e) {
+            System.err.println("Error: " + e.getMessage());
+        }
     }
 }
