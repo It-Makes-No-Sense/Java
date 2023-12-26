b6335afd94d232438e92b9163e16a1d3bc63d065
Grisha-NoSense <Devimp15@mail.ru>
2023-12-26 13:45:39 +0300

    Write main code.

--- FinalWork/Main.java
+++ FinalWork/Main.java
@@ -1,8 +1,28 @@
 package FinalWork;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-
-
+        ToysDispenser toysDispenser = new ToysDispenser();
+        Scanner sc = new Scanner(System.in);
+        while (true) {
+            System.out.print("""
+                    Меню:
+                    1 - Добавить новую игрушку
+                    2 - Провести розыгрыш с сохранением результата
+                    3 - Выход
+                    >\s""");
+            var selection = sc.next();
+            switch (selection) {
+                case "1" -> toysDispenser.addToy();
+                case "2" -> toysDispenser.playToys();
+                case "3" -> {
+                    System.out.println("До новых встреч!");
+                    System.exit(4);
+                }
+                default -> System.out.println("Некорректный выбор. Попробуйте снова.");
+            }
+        }
     }
 }
